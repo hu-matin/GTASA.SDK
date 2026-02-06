589584954242d31ee0956c0cb38be511cc5ab261
MahdiKarami <67632452+mahdikarami8484@users.noreply.github.com>
2026-02-06 11:13:43 +0330

    Add HookManager for add or remove hook.

---
+++ src/core/hook/hook_manager.cpp
@@ -0,0 +1,37 @@
+#include "hook_manager.h"
+#include <core/logging/logger.hpp>
+
+using namespace Logging;
+
+HookManager& HookManager::instance()
+{
+    static HookManager instance;
+    return instance;
+}
+
+void HookManager::addHook(void** target, void* detour) {
+    this->hooks.push_back({ target, detour });
+}
+
+void HookManager::enableHooks() {
+    DetourTransactionBegin();
+    DetourUpdateThread(GetCurrentThread());
+
+    for (auto& hook : hooks) {
+        DetourAttach(hook.target, hook.detour);
+    }
+
+    DetourTransactionCommit();
+    LOG_INFO("[HookManager] hooks enabled.");
+}
+
+void HookManager::disableHooks() {
+    DetourTransactionBegin();
+    DetourUpdateThread(GetCurrentThread());
+
+    for (auto& hook : hooks) {
+        DetourDetach(hook.target, hook.detour);
+    }
+
+    DetourTransactionCommit();
+}
