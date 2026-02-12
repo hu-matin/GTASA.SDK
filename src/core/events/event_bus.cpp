81f52b823bd04e3003cb6033a83b71ad02d677ba
MahdiKarami <67632452+mahdikarami8484@users.noreply.github.com>
2026-02-12 18:28:43 +0330

    add event system

---
+++ src/core/events/event_bus.cpp
@@ -0,0 +1,22 @@
+#include "event_bus.h"
+
+using namespace GTASA::SDK;
+
+EventBus& EventBus::instance() {
+	static EventBus instance;
+	return instance;
+}
+
+void EventBus::add(std::unique_ptr<Script> script)
+{
+	if (!script) return;
+	m_scripts.emplace_back(std::move(script));
+}
+
+void EventBus::dispatch(EventType type) {
+	for (auto& script : m_scripts) {
+		if (script && script->isEnabled()) {
+			script->onEvent(type);
+		}
+	}
+}
---
+++ src/core/events/event_type.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace GTASA {
+	namespace SDK {
+		enum class EventType {
+			GameProcess,
+			Initialize,
+			Shutdown
+		};
+	}
+}
+
