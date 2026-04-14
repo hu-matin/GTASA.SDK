0c5b646965388840befd74179409937526583415
MahdiKarami <67632452+mahdikarami8484@users.noreply.github.com>
2026-04-14 16:05:15 +0330

    use playerInfo

--- scripts/sample_script.cpp
+++ scripts/sample_script.cpp
@@ -4,9 +4,9 @@
 #include "core/event/event_bus.h"
 #include "core/logging/logger.hpp"
 #include "core/script/auto_register_script.h"
-#include "core/game/player.h"
 #include "hooks/game_loop_hook.h"
 #include "hooks/player_hook.h"
+#include "core/game/player_info.h"
 #include <string>
 
 using namespace Logging;
@@ -25,28 +25,35 @@ namespace GTASA {
                 if (const auto* processFrameEvent = dynamic_cast<const Events::ProcessFrameEvent*>(event.get())) {
                     if ((GetAsyncKeyState(VK_F5) & 1))
                     {
-                        std::unique_ptr<Player> player = Player::getLocal();
-                        if(!player || !player->isValid())
+                        std::unique_ptr<PlayerInfo> playerInfo = PlayerInfo::getLocal();
+                        if(!playerInfo)
                             return;
-                        player->setHealth(1000.0f);
-                        player->setArmor(1000.0f);
-                        float hp = player->getHealth();
-                        float arm = player->getArmor();
+                        playerInfo->getPed()->setHealth(1000.0f);
+                        playerInfo->getPed()->setArmor(1000.0f);
+                        float hp = playerInfo->getPed()->getHealth();
+                        float arm = playerInfo->getPed()->getArmor();
                         LOG_INFO("Player Health: %.2f", hp);
                         LOG_INFO("Player Armor: %.2f", arm);
 
-                        Vector3 pos = player->getMatrix()->getPosition();
+                        Vector3 pos = playerInfo->getPed()->getMatrix()->getPosition();
                         LOG_INFO("Player pos_x: %.2f, pos_y: %.2f, pos_z:%.2f", 
                         pos.x, pos.y, pos.z);
 
-                        player->getMatrix()->setPosition({pos.x, pos.y, pos.z + 5.0f});
+                        playerInfo->getPed()->getMatrix()->setPosition({pos.x, pos.y, pos.z + 5.0f});
+
+                        playerInfo->setMoney(200);
+                        int wantedLevel = playerInfo->getWantedLevel();
+                        playerInfo->setWantedLevel(wantedLevel > 0 ? 0 : 6);
+
+                        int chaos = playerInfo->getChaos();
+                        wantedLevel = playerInfo->getWantedLevel();
+                        LOG_INFO("chaos:%d, wantedLevel: %d", chaos, wantedLevel);
                     }
                 }
                 
-                // if(const auto* crimeEvent = dynamic_cast<const Events::CrimeCommittedEvent*>(event.get())) {
-                //     LOG_INFO("[Sample Script] crime event...");
-                // }
-
+                if(const auto* crimeEvent = dynamic_cast<const Events::CrimeCommittedEvent*>(event.get())) {
+                    LOG_INFO("[Sample Script] crime event...");
+                }
                 
             }
         };
