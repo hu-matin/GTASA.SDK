9c2bcd5439da708eeb453aea9821a0f25bff088f
MahdiKarami <67632452+mahdikarami8484@users.noreply.github.com>
2026-04-14 16:05:58 +0330

    add PlayerInfo offset

--- src/core/game/offsets.h
+++ src/core/game/offsets.h
@@ -7,9 +7,16 @@ namespace GTASA {
         namespace Offsets
         {
             namespace Globals {
-                constexpr uintptr_t LocalPlayer = 0x0076F3B8;
+                constexpr uintptr_t LocalPlayer = 0x77CD98; // 0x0076F3B8;
             }
-            
+
+            namespace PlayerInfo {
+                constexpr uintptr_t Ped = 0x00;
+                constexpr uintptr_t Money = 0xB8;       // 0x77CE50; // 0x77CE50 - 0x77CD9C = 180
+                constexpr uintptr_t Chaos = 0x4; // TODO: 
+                constexpr uintptr_t WantedLevel = 0x2C;
+            }
+
             namespace CPed {
                 constexpr uintptr_t Health = 0x540;
                 constexpr uintptr_t Armor = 0x548;
