b25db86bae16b621dda41495758e6bef4eb46a67
MahdiKarami <67632452+mahdikarami8484@users.noreply.github.com>
2026-04-29 21:53:55 +0330

    remove redundant comments and tidy up the file

--- src/core/game/ped.h
+++ src/core/game/ped.h
@@ -8,11 +8,6 @@ namespace GTASA {
             class Ped : public Entity {
             public:
                 using Entity::Entity;
-                // 117027A8 - 0x540 = 11702268
-                /*
-                    1186B038
-                    1170227C - 11702268 = 0x14
-                */
                 std::unique_ptr<Matrix> getMatrix() const;
 
                 
